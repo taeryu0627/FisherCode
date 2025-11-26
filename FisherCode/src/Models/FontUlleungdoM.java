6570708c770b19af3e96edd0db79e53a80d618fb
noilebx <noilebx@gmail.com>
2025-11-26 21:57:56 +0900

    Add files via upload

---
+++ FisherCode/src/Models/FontUlleungdoM.java
@@ -0,0 +1,33 @@
+package Models;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+
+public class FontUlleungdoM {
+    
+    private static Font customFont = null;
+    
+    private static final String FONT_PATH = "src/resources/fonts/HY울릉도M.ttf";
+
+    public static Font getCustomFont(float size) {
+        if (customFont == null) {
+            try {
+                // 폰트 파일에서 폰트 로드
+                customFont = Font.createFont(
+                    Font.TRUETYPE_FONT, 
+                    new File(FONT_PATH)
+                );
+            } catch (IOException | FontFormatException e) {
+                // 오류 발생 시 맑은 고딕 사용
+                System.err.println("폰트 로드 실패 기본 폰트를 사용함");
+                e.printStackTrace();
+                customFont = new Font("맑은 고딕", Font.PLAIN, (int) size); 
+            }
+        }
+        
+        // 폰트를 지정된 크기로 출력
+        return customFont.deriveFont(size);
+    }
+}
